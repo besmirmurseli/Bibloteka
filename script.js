6811479d10770c2c80ba0310653eae21b524e115
besmirmurseli <besmir.murseli14@gmail.com>
2021-12-02 13:42:10 +0100

    commit

---
+++ script.js
@@ -0,0 +1,30 @@
+let Username = "Besi"; //string 
+let uerage = "18"; //number
+let birth_year = "2002";
+let isKid = false;
+let isAdult = true;
+let height = 1.80; 
+let temperature = -20; 
+let quotes = "Life is Great!";
+let user_email; //undefined - ( e padefinuar )
+let score = null; //e zbrazet
+
+
+// funksion i gatshem i JS 
+
+console.log("Hello World");
+
+
+console.log(quotes)
+console.log("quotes")
+quotes = "Life is not Great Anymore";
+
+console.log(quotes);
+alert("Ku je nis o Bab!");
+let username = prompt("Sheno Emrin tuaj.");
+let userSurname = prompt("Sheno Mbiemrin tuaj.");
+let user_age = prompt("Sheno Moshen tuaj");
+let Emri  = prompt("Sheno Emrin e plotë");
+let Mosha = prompt("Sheno Moshën tuaj ne formën e shkruar")
+
+console.log(username, userSurname, user_age, Emri, Mosha  )
